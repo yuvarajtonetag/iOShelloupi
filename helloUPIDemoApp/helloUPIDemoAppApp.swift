42f99768fa08c831643a05f18ad28a241fc74b4c
Jayasurya <jayasurya.g@cumulations.com>
2025-01-08 12:27:36 +0530

    Initial Commit

---
+++ helloUPIDemoApp/helloUPIDemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  helloUPIDemoAppApp.swift
+//  helloUPIDemoApp
+//
+//  Created by cumulations on 08/01/25.
+//
+
+import SwiftUI
+
+@main
+struct helloUPIDemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
