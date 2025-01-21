be8eab8242b21ac63a295f23fdf273a6b24b2276
Jayasurya <jayasurya.g@cumulations.com>
2025-01-21 13:03:39 +0530

    - Hello UPI ios Framework is addded

---
+++ Hello_UPI_iOS_Framework.framework/Headers/Hello_UPI_iOS_Framework.h
@@ -0,0 +1,18 @@
+//
+//  Hello_UPI_iOS_Framework.h
+//  Hello UPI iOS Framework
+//
+//  Created by Narayan Shettigar on 22/07/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Hello_UPI_iOS_Framework.
+FOUNDATION_EXPORT double Hello_UPI_iOS_FrameworkVersionNumber;
+
+//! Project version string for Hello_UPI_iOS_Framework.
+FOUNDATION_EXPORT const unsigned char Hello_UPI_iOS_FrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Hello_UPI_iOS_Framework/PublicHeader.h>
+
+
