2e04365d14f68ae0ea56a84d6637e7d05ef95e8a
Jayasurya <jayasurya.g@cumulations.com>
2025-02-05 20:56:54 +0530

    - older version support

---
+++ helloUPIDemoApp/helloUPIDemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  helloUPIDemoAppApp.swift
+//  helloUPIDemoApp
+//
+//  Created by JAYA$URYA on 05/02/25.
+//
+
+import SwiftUI
+
+@main
+struct helloUPIDemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeScreen()
+        }
+    }
+}
